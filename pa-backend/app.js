66ccd2c96403f73c648d0386fa47abb21e421994
jacksonliiii <jacksonli2001@hotmail.com>
2023-06-02 16:13:51 -0700

    Fix refresh bug

--- pa-backend/app.js
+++ pa-backend/app.js
@@ -3,6 +3,7 @@
  *******************************************************************************/
 const express = require("express");
 const app = express();
+const path = require("path");
 
 require("express-async-errors");
 const cors = require("cors");
@@ -43,10 +44,10 @@ app.use("/api/tickets", ticketsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
-// if (process.env.NODE_ENV === "test") {
-//   const testingRouter = require("./controllers/testing");
-//   app.use("/api/testing", testingRouter);
-// }
+// Catch-all route handler
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
