31f69ec3aef4f2c6ae1f584ea833ce9f56f988f1
jacksonliiii <jacksonli2001@hotmail.com>
2023-06-02 15:22:35 -0700

    Modify for deployment

--- pa-backend/app.js
+++ pa-backend/app.js
@@ -35,6 +35,7 @@ mongoose
  *******************************************************************************/
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
